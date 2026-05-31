8c480557c79002059a6d21797c40d32fa4cfdeba
SHAIK MUMINA <muminashaik2006@gmail.com>
2026-05-31 20:20:33 +0530

    Add smart_bot function for interactive chatbot
    Implemented a rule-based chatbot with predefined responses based on user input.

--- Task1_Chatbot/chatbot.py
+++ Task1_Chatbot/chatbot.py
@@ -1,13 +1,38 @@
-Rule-Based ChatBot
-This project is developed as part of the CodSoft Artificial Intelligence Internship Program.
-The chatbot is designed to communicate with users through predefined conversational rules and provide appropriate responses based on user input.
-Features
-Interactive text-based conversation
-Rule-based response system
-Simple and user-friendly interface
-Exit command to end the chat
-Technologies Used
-Python
-Conditional Statements
-Loops and Functions
-Project Status
+def smart_bot():
+
+    print("Welcome to Smart Assistant")
+    print("Type 'terminate' to stop the chat.\n")
+
+    while True:
+
+        visitor_input= input("You: ").strip().lower()
+
+        if visitor_input in ["hi", "hello", "hey"]:
+            print("Assistant: Hello! Nice to meet you.")
+
+        elif "name" in visitor_input:
+            print("Assistant: I am Smart Assistant.")
+
+        elif "college" in visitor_input:
+            print("Assistant: I can help with simple questions.")
+
+        elif "course" in visitor_input:
+            print("Assistant: Artificial Intelligence is an exciting field.")
+
+        elif "time" in visitor_input:
+            print("Assistant: Sorry, I cannot check the current time.")
+
+        elif "thank you" in visitor_input or "thanks" in visitor_input:
+            print("Assistant: You're welcome!")
+            
+        elif "bye"   in  visitor_input:
+            print("Assistant: Good night")
+
+        elif visitor_input == "terminate":
+            print("Assistant: Chat terminated successfully.")
+            break
+
+        else:
+            print("Assistant: I don't have an answer for that question.")
+
+smart_bot()
